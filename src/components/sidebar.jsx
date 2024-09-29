b70de2d13880dd8fdff0843139fb5bf98344649b
Elif Aslan <elfaslnvon@gmail.com>
2024-09-29 18:25:43 +0300

    table added

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -18,6 +18,13 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import ImageIcon from "@mui/icons-material/Image";
+import WorkIcon from "@mui/icons-material/Work";
+import BeachAccessIcon from "@mui/icons-material/BeachAccess";
 
 const drawerWidth = 240;
 
@@ -29,6 +36,17 @@ const openedMixin = (theme) => ({
   }),
   overflowX: "hidden",
 });
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles("dark", {
+    backgroundColor: "#1A2027",
+  }),
+}));
 
 const closedMixin = (theme) => ({
   transition: theme.transitions.create("width", {
@@ -112,135 +130,173 @@ function Sidebar() {
   };
   return (
     <>
-      <Box sx={{ display: "flex", margin: 0, padding: 0 }}>
-        <CssBaseline />
-        {/* <AppBar position="fixed" open={open}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
-              sx={[
-                {
-                  marginRight: 5,
-                // },
-                open && { display: "none" },
-              ]}
-            >
-              <MenuIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar> */}
-        <Drawer
-          variant="permanent"
-          open={open}
-          sx={{
-            "& .MuiDrawer-paper": {
-              backgroundColor: "rgb(243, 210, 111)",
-            },
-          }}
-        >
-          <DrawerHeader>
-            <Typography sx={[open == false && { display: "none" }]}>
-              TODO
-            </Typography>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
-              sx={[
-                {
-                  minHeight: 48,
-                  px: 2.5,
-                },
-                open && { display: "none" },
-              ]}
-            >
-              <MenuIcon />
-            </IconButton>
-            <IconButton
-              onClick={handleDrawerClose}
-              sx={[
-                {
-                  minHeight: 48,
-                  px: 2.5,
-                },
-                open == false && { display: "none" },
-              ]}
-            >
-              {/* To-Do */}
-              <ChevronLeftIcon />
-            </IconButton>
-          </DrawerHeader>
-          <Divider />
-          <List>
-            {["Today", "Important", "Notes"].map((text, index) => (
-              <ListItem key={text} disablePadding sx={{ display: "block" }}>
-                <ListItemButton
-                  sx={[
-                    {
-                      minHeight: 48,
-                      px: 2.5,
-                    },
-                    open
-                      ? {
-                          justifyContent: "initial",
-                        }
-                      : {
-                          justifyContent: "center",
-                        },
-                  ]}
-                >
-                  <ListItemIcon
+      <CssBaseline />
+      <Grid container sx={{ height: "100vh" }}>
+        <Grid item>
+          <Drawer
+            variant="permanent"
+            open={open}
+            sx={{
+              "& .MuiDrawer-paper": {
+                backgroundColor: "rgb(243, 210, 111)",
+              },
+            }}
+          >
+            <DrawerHeader>
+              <Typography sx={[open == false && { display: "none" }]}>
+                TODO
+              </Typography>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                sx={[
+                  {
+                    minHeight: 48,
+                    px: 2.5,
+                  },
+                  open && { display: "none" },
+                ]}
+              >
+                <MenuIcon />
+              </IconButton>
+              <IconButton
+                onClick={handleDrawerClose}
+                sx={[
+                  {
+                    minHeight: 48,
+                    px: 2.5,
+                  },
+                  open == false && { display: "none" },
+                ]}
+              >
+                {/* To-Do */}
+                <ChevronLeftIcon />
+              </IconButton>
+            </DrawerHeader>
+            <Divider />
+            <List>
+              {["Today", "Important", "Notes"].map((text, index) => (
+                <ListItem key={text} disablePadding sx={{ display: "block" }}>
+                  <ListItemButton
                     sx={[
                       {
-                        minWidth: 0,
-                        justifyContent: "center",
+                        minHeight: 48,
+                        px: 2.5,
                       },
                       open
                         ? {
-                            mr: 3,
+                            justifyContent: "initial",
                           }
                         : {
-                            mr: "auto",
+                            justifyContent: "center",
                           },
                     ]}
                   >
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText
-                    primary={text}
-                    sx={[
-                      open
-                        ? {
-                            opacity: 1,
-                          }
-                        : {
-                            opacity: 0,
-                          },
-                    ]}
-                  />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          {/* <Divider /> */}
-        </Drawer>
-        <div
-          className="container"
-          style={{
-            backgroundColor: "pink",
-            margin: 0,
-            padding: 0,
-            height: "100vh",
+                    <ListItemIcon
+                      sx={[
+                        {
+                          minWidth: 0,
+                          justifyContent: "center",
+                        },
+                        open
+                          ? {
+                              mr: 3,
+                            }
+                          : {
+                              mr: "auto",
+                            },
+                      ]}
+                    >
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={text}
+                      sx={[
+                        open
+                          ? {
+                              opacity: 1,
+                            }
+                          : {
+                              opacity: 0,
+                            },
+                      ]}
+                    />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
+        </Grid>
+
+        <Grid
+          sx={{
+            flexGrow: 1,
+            backgroundColor: "rgb(252, 239, 190)",
           }}
         >
-          {" "}
-          hello world
-        </div>
-      </Box>
+          <Grid
+            container
+            rowSpacing={0}
+            sx={{ display: "flex", justifyContent: "center" }}
+          >
+            <Grid item xs={12} sx={{ backgroundColor: "none" }}>
+              <Item>TITLE</Item>
+            </Grid>
+            <Grid item xs={8} sx={{ marginTop: "100px" }}>
+              <Item sx={{ backgroundColor: "rgb(93, 127, 15)" }}>
+                <List
+                  sx={{
+                    width: "100%",
+                  }}
+                >
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <ImageIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <WorkIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary="Work" secondary="Jan 7, 2014" />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <BeachAccessIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Vacation"
+                      secondary="July 20, 2014"
+                    />
+                  </ListItem>
+                </List>
+              </Item>
+            </Grid>
+          </Grid>
+          {/* <div
+            className="main"
+            style={{
+              height: "75%",
+              width: "75%",
+              display: "flex",
+              backgroundColor: "white",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            asdf
+          </div> */}
+        </Grid>
+      </Grid>
     </>
   );
 }
